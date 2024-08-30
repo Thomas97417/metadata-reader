9d935d116dcc77488f5c280dff23180cf146c714
TOMA <throuquette@hotmail.fr>
2024-08-30 15:59:29 +0200

    updated pre tags & ImageDetails inside ratio

--- components/ImageDetails.tsx
+++ components/ImageDetails.tsx
@@ -13,7 +13,7 @@ export default function ImageDetails({
   return (
     <div className="flex w-full">
       {imageUrl && (
-        <div className="flex-1 flex-col">
+        <div className="w-1/2 flex flex-col">
           <img
             src={imageUrl}
             alt="uploaded"
@@ -25,9 +25,9 @@ export default function ImageDetails({
         </div>
       )}
       {metadata !== null && (
-        <div className="flex-1">
+        <div className="w-1/2">
           <p className="font-bold text-lg">Metadata:</p>
-          <pre className="max-w-full overflow-x-auto ">
+          <pre className="max-w-full overflow-x-auto whitespace-pre-wrap">
             {metadata !== undefined
               ? JSON.stringify(metadata, null, 2)
               : "This image has no metadata."}
