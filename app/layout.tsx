6cbfb6e006701f9b66efccde8ff19b35101a9da7
TOMA <throuquette@hotmail.fr>
2024-08-31 17:03:31 +0200

    add Analytics to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
