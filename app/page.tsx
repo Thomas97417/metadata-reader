7574bffdb76bf619b5933cfc9fc94a3acc251549
TOMA <throuquette@hotmail.fr>
2026-03-22 17:03:55 +0100

    improve ui and ux of default page

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,14 @@ import { motion } from "framer-motion";
 export default function MetadataPage() {
   return (
     <div className="bg-background relative pt-16">
-      {/* Content */}
       <div className="relative z-10 container mx-auto max-w-7xl">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-          className="px-4 space-y-8"
+          className="px-4"
         >
-          {/* Details Section */}
-          <div className="pb-16">
-            <ImageDetails />
-          </div>
+          <ImageDetails />
         </motion.div>
       </div>
     </div>
